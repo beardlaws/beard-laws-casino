c8f0e4fa78b8af91134e51dae4a265510c2eca92
beardlaws <officialbeardlaws@gmail.com>
2026-07-30 14:27:14 -0400

    delete

--- src/games/BeardBank/BeardBankConfig.js
+++
@@ -1,5 +0,0 @@
-const common=['oil','comb','razor','balm','oil','key','comb','oil','razor','balm','crown','oil','coin','comb','razor','oil','vault','balm','key','oil'];
-export const beardBankConfig={
-  id:'beard-bank',title:'Beard Bank',reels:5,rows:3,
-  reelStrips:[common,common,[...common,'vernon','comb','coin','oil','key','balm'],common,common]
-};
